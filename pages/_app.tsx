8ea512bd3dd5390f7005df7b86f21b931eca9354
iVeGa123 <vitor.da.alvorada@gmail.com>
2024-09-15 12:09:00 -0400

    Fix some issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,14 @@ import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
 import { theme } from '../theme';
 import Link from 'next/link';
+import '@ptkdev/webcomponent-patreon-box';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider defaultColorScheme='dark' theme={theme}>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet" />
         <title>Anime Euphoria</title>
         <meta
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,53 @@
-import {
-  Paper,
-  TextInput,
-  PasswordInput,
-  Checkbox,
-  Button,
-  Title,
-  Text,
-  Anchor,
-} from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { Paper, Title, TextInput, PasswordInput, Checkbox, Button, Text, Anchor } from '@mantine/core';
 import classes from './AuthenticationImage.module.css';
-import { icons } from '@tabler/icons-react';
-
 
 export default function HomePage() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // Coisas que vão carregar só no lado do cliente
+  }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      // Carrega o Script do Patreon para redirecionar
+      const script = document.createElement('script');
+      script.src = "https://c6.patreon.com/becomePatronButton.bundle.js";
+      script.async = true;
+      document.body.appendChild(script);
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
+  }, [isClient]);
+
   return (
     <div className={classes.wrapper}>
       <Paper className={classes.form} shadow="xl" radius={"xl"} p={"xl"}>
         <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
           Bem vindo ao Anime Euphoria!
         </Title>
-        <TextInput classNames={{ label: classes.textInputLabel, input: classes.textInputInput }}  label="Endereço de email" placeholder="hello@gmail.com" size="md" />
-        <PasswordInput classNames={{ label: classes.textInputLabel, input: classes.textInputInput}} label="Senha" placeholder="Sua senha" mt="md" size="xl" />
-        <Checkbox classNames={{labelWrapper: classes.textInputLabel, inner: classes.textCheckboxIcon}} label="Mantenha-me logado" mt="xl" size="md" />
-        <Button classNames={{label: classes.buttonLabel, root: classes.buttonRoot}} fullWidth mt="xl" size="md">
+        <TextInput
+          classNames={{ label: classes.textInputLabel, input: classes.textInputInput }}
+          label="Endereço de email"
+          placeholder="hello@gmail.com"
+          size="md"
+        />
+        <PasswordInput
+          classNames={{ label: classes.textInputLabel, input: classes.textInputInput }}
+          label="Senha"
+          placeholder="Sua senha"
+          mt="md"
+          size="xl"
+        />
+        <Checkbox
+          classNames={{ labelWrapper: classes.textInputLabel, inner: classes.textCheckboxIcon }}
+          label="Mantenha-me logado"
+          mt="xl"
+          size="md"
+        />
+        <Button classNames={{ label: classes.buttonLabel, root: classes.buttonRoot }} fullWidth mt="xl" size="md">
           Logar
         </Button>
         <Text className={classes.AccountText} ta="center" mt="md">
@@ -30,14 +55,20 @@ export default function HomePage() {
           <Anchor<'a'> href="#" fw={700} onClick={(event) => event.preventDefault()} className={classes.AccountText}>
             Registre-se
           </Anchor>
-          <Button classNames={{label: classes.buttonLabel, root: classes.buttonRoot2}} fullWidth mt="xl" size="md">
-          Logar com Facebook
-        </Button>
         </Text>
+
+        {isClient && (
+          <a href="https://www.patreon.com/bePatron?u=31624754" data-patreon-widget-type="become-patron-button">
+            Become a member!
+          </a>
+        )}
       </Paper>
-      <video id="video" className={classes.video} autoPlay loop muted playsInline>
-        <source src="./4k.mp4" type="video/mp4" />
-      </video>
+
+      {isClient && (
+        <video id="video" className={classes.video} autoPlay loop muted playsInline>
+          <source src="./4k.mp4" type="video/mp4" />
+        </video>
+      )}
     </div>
   );
 }
